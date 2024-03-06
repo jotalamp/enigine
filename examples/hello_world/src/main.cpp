8f133db06f9292bc219bb825a965eef2a96f2d11
Jotalamp <joonaslampinen1@gmail.com>
2024-03-06 17:27:32 +0000

    added rim and sky to hello_world application

--- examples/hello_world/src/main.cpp
+++ examples/hello_world/src/main.cpp
@@ -42,7 +42,7 @@ int main()
     eTransform transform;
     transform.setPosition(glm::vec3(0.0f, 0.0f, 0.0f));
     transform.setScale(glm::vec3(2.0f));
-    Model &shelter = *resourceManager->getModel("assets/models/cylinder.obj");
+    Model &shelter = *resourceManager->getModel("assets/models/rim.obj");
     renderManager->addSource(RenderSourceBuilder()
                                  .setTransform(transform)
                                  .setModel(&shelter)
@@ -51,7 +51,7 @@ int main()
     // vehicle
     Models models;
     models.carBody = resourceManager->getModel("assets/models/cylinder.obj");
-    models.wheelBase = resourceManager->getModel("assets/models/cylinder.obj");
+    models.wheelBase = resourceManager->getModel("assets/models/eim.obj");
     models.doorFront = resourceManager->getModel("assets/models/cylinder.obj");
     models.smokeParticleModel = resourceManager->getModel("assets/models/cylinder.obj");
 
@@ -80,49 +80,28 @@ int main()
     // door offset - when opened
     vehicle.setActiveDoorHingeOffsetFront(glm::vec3(0.8f, 1.17f, 0.8f), glm::vec3(0.6f, 0.0f, 0.5f));
 
-    // TODO: single source
-    /*
-    car.m_linkDoors[0]->m_offsetActive.setPosition(glm::vec3(-0.2f, -0.8f, -0.67f));
-    car.m_linkDoors[0]->m_offsetActive.setRotation(glm::quat(0.5f, 0.5f, 0.5f, 0.5f));
-    vehicle.m_doors[0].rigidbodyOffset.setPosition(glm::vec3(0.8f, 0.67f, 0.2f));
-    vehicle.m_doors[0].rigidbodyOffset.setRotation(glm::quat(0.5f, -0.5f, -0.5f, -0.5f));
+    // set environment map
+    std::string path = resourceManager->m_executablePath + "/assets/hdris/qwantani_puresky.hdr";
+    TextureParams textureParams;
+    textureParams.dataType = TextureDataType::Float32;
+    Texture envTexture = resourceManager->textureFromFile(textureParams, path, path);
+    renderManager->updateEnvironmentTexture(envTexture);
+    renderManager->m_shadowManager->m_lightPos = glm::normalize(glm::vec3(0.693f, 0.51f, 0.51f));
 
-    car.m_linkDoors[1]->m_offsetActive.setPosition(glm::vec3(-0.2f, 0.8f, -0.67f));
-    car.m_linkDoors[1]->m_offsetActive.setRotation(glm::quat(0.5f, 0.5f, 0.5f, 0.5f));
-    vehicle.m_doors[1].rigidbodyOffset.setPosition(glm::vec3(-0.8f, 0.67f, 0.2f));
-    vehicle.m_doors[1].rigidbodyOffset.setRotation(glm::quat(0.5f, -0.5f, -0.5f, -0.5f));
+    // add render source
+    std::string modelPath = resourceManager->m_executablePath + "/assets/models/rim.glb";
+    Model *rimModel = resourceManager->getModelFullPath(modelPath);
+    //eTransform transform;
+    transform.setScale(glm::vec3(2.f));
+    RenderSource *rimSource = RenderSourceBuilder()
+                                  .setTransform(transform)
+                                  .setModel(rimModel)
+                                  .build();
+    renderManager->addSource(rimSource);
 
-    // TODO: cull face front because of mirrored
-    car.m_linkDoors[1]->m_offsetActive.setScale(glm::vec3(-1.f, 1.f, 1.f));
-    car.m_linkDoors[1]->m_offsetDeactive.setScale(glm::vec3(-1.f, 1.f, 1.f));
-
-    car.m_linkWheels[0]->m_offset.setScale(glm::vec3(1.f, 1.12f, 1.12f));
-    car.m_linkWheels[1]->m_offset.setScale(glm::vec3(1.f, 1.12f, 1.12f));
-    car.m_linkWheels[2]->m_offset.setScale(glm::vec3(1.f, 1.18f, 1.18f));
-    car.m_linkWheels[3]->m_offset.setScale(glm::vec3(1.f, 1.18f, 1.18f));
-
-    // door collider
-    glm::vec3 size(0.6, 0.15, 0.4);
-    vehicle.m_doors[0].body->getCollisionShape()->setLocalScaling(BulletGLM::getBulletVec3(size));
-    vehicle.m_doors[1].body->getCollisionShape()->setLocalScaling(BulletGLM::getBulletVec3(size));
-
-    // exhaust
-    car.m_linkExhausts[0]->m_offset.setPosition(glm::vec3(0.32f, 1.f, -1.02f));
-    car.m_linkExhausts[0]->m_offset.setRotation(glm::quat(0.f, 1.f, 0.f, 0.f));
-    car.m_linkExhausts[1]->m_offset.setPosition(glm::vec3(-0.32f, 1.f, -1.02f));
-    car.m_linkExhausts[1]->m_offset.setRotation(glm::quat(0.f, 1.f, 0.f, 0.f));
-
-    car.m_controlVehicle = true;
-    car.m_followVehicle = true;
-    inputManager->addKeyListener(std::bind(&CarController::keyListener, &car,
-                                           std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4, std::placeholders::_5));
-
-    // TODO: before character - parent transform - ?
-    updateManager->add(&car);
-
-    VehicleUI *vehicleUI = new VehicleUI(&car, &vehicle);
-    enigine.rootUI->m_uiList.push_back(vehicleUI);
-    */
+    // update camera
+    mainCamera->position = glm::vec3(-2.f, 0.f, 2.f);
+    mainCamera->front = glm::vec3(0.707f, 0.f, -0.707f);
 
     // start main loop
     enigine.start();
